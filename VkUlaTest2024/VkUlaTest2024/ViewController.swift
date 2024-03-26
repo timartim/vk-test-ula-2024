d3526bcb561f125ef8f72e188ff9c37edb58e505
Artemiy <timtimk30@yandex.ru>
2024-03-26 22:18:52 +0300

    Initial commit

---
+++ VkUlaTest2024/VkUlaTest2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkUlaTest2024
+//
+//  Created by Артемий on 26.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
